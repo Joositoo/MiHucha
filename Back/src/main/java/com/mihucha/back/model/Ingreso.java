52614647a9c8459209af1a0929b696a1f85b4e7f
Joositoo <jmepk115@gmail.com>
2026-01-20 19:09:21 +0100

    Create entities

---
+++ Back/src/main/java/com/mihucha/back/model/Ingreso.java
@@ -0,0 +1,78 @@
+package com.mihucha.back.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ingresos")
+public class Ingreso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario_id;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria_id;
+
+    private BigDecimal cantidad;
+
+    private String descripcion;
+
+    private LocalDateTime fecha;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(Usuario usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public Categoria getCategoria_id() {
+        return categoria_id;
+    }
+
+    public void setCategoria_id(Categoria categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    public BigDecimal getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(BigDecimal cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+}
